018929734713dc4d08de27844f3eb4b05983d3d8
MarianaZalyciailo <zalyciailo@gmail.com>
2022-07-25 12:55:42 +0300

    Initial commit

---
+++ Lessons1/l1.js
@@ -0,0 +1,52 @@
+let a = 'hello';
+console.log(a);
+alert(a);
+document.write(a);
+let b = 'owu';
+console.log(b);
+alert(b);
+document.write(b);
+let c = 'com';
+console.log(c);
+alert(c);
+document.write(c);
+let d = 'ua';
+console.log(d);
+alert(d);
+document.write(d);
+let a1 = 1 ;
+console.log(a1);
+alert(a1);
+document.write(a1);
+let a10 = 10 ;
+console.log(a10);
+alert(a10);
+document.write(a10);
+let number = -999 ;
+console.log(number);
+alert(number);
+document.write(number);
+let a123 = 123 ;
+console.log(a123);
+alert(a123);
+document.write(a123);
+let number2 = 3.14 ;
+console.log(number2);
+alert(number2);
+document.write(number2)
+let number3 = 2.7 ;
+console.log(number3);
+alert(number3);
+document.write(number3)
+let number4 = 16 ;
+console.log(number4);
+alert(number4);
+document.write(number4)
+let istrue=true;
+alert(istrue);
+document.write(istrue);
+let isfolse=false;
+alert(isfolse);
+document.write(isfolse);
+
+
